1e66084eea6b607d29c971d9b78937ec7567dcac
Joan Bosch <jbosch@adiquimica.com>
2023-02-05 17:26:30 +0100

    Add courses

--- components/entry.tsx
+++ components/entry.tsx
@@ -1,21 +1,24 @@
 export default function Entry ({ info })
 {
   return (
-    <div className="group relative col-span-full flex flex-col items-center gap-2 overflow-hidden rounded bg-gradient-to-r from-gray-850 to-gray-900 p-3 pt-4 text-center shadow-2xl shadow-black/45 md:col-span-8 md:flex-row md:items-start md:pt-3 md:text-left lg:gap-8 lg:p-4 transition ease-in-out delay-0 hover:shadow-orange-200">
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 text-black">{info.title}</div>
-        <p className="text-gray-700 text-base">
-          {info.description}
-        </p>
+    <a href={info.link}>
+      <div className="group relative col-span-full flex flex-col items-center gap-2 overflow-hidden bg-gray-100 rounded bg-gradient-to-r from-gray-850 to-gray-900 p-3 pt-4 text-center shadow-2xl shadow-black/45 md:col-span-8 md:flex-row md:items-start md:pt-3 md:text-left lg:gap-8 lg:p-4 transition ease-in-out delay-5 hover:shadow-orange-200">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-black">{info.title}</div>
+          <div className="mb-2 text-xs text-black">{info.date}</div>
+          <p className="text-gray-700 text-base">
+            {info.description}
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2 text-right">
+          {
+            info.tags.map((tag) => (
+              // eslint-disable-next-line react/jsx-key
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
+            ))
+            }
+        </div>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        {
-          info.tags.map((tag) => (
-            // eslint-disable-next-line react/jsx-key
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{tag}</span>
-          ))
-        }
-      </div>
-    </div>
+    </a>
   )
 }
--- locales/es.js
+++ locales/es.js
@@ -2,16 +2,18 @@ const es = {
   locale: 'es',
   courses: [
     {
-      title: 'Curso 1',
-      date: '2019-01-01',
-      description: 'Descripcion curso 1',
-      tags: ['tag1', 'tag2']
+      title: 'Curso Introducción a la programación con Python',
+      date: 'Noviembre 2022',
+      description: 'Curso de introducción a la programación utilizando el lenguaje Python. Se enseñan los conceptos básicos de programación y se realizan ejercicios prácticos para aprender a programar.',
+      tags: ['programación', 'python', 'educación'],
+      link: 'https://python.learn.joanbosch.dev'
     },
     {
-      title: 'Curso 2',
-      date: '2019-01-02',
-      description: 'Descripcion curso 2',
-      tags: ['tag1', 'tag2']
+      title: 'Actividades de realidad aumentada con Merge Cube',
+      date: 'Junio 2022',
+      description: 'Actividades didácticas para alumnos de Educación Secundaria Obligatoria utilizando Merge Cube, un dispositivo de realidad aumentada que permite visualizar modelos 3D en el mundo real.',
+      tags: ['AR', 'educación'],
+      link: 'https://upcommons.upc.edu/handle/2117/369344'
     }
   ]
 }
