931c86e4120a610de78f1f80d7dac3fcebbc3eae
Joan Bosch <jbosch@adiquimica.com>
2023-02-05 17:57:25 +0100

    Fix python course

--- locales/es.js
+++ locales/es.js
@@ -6,7 +6,7 @@ const es = {
       date: 'Noviembre 2022',
       description: 'Curso de introducción a la programación utilizando el lenguaje Python. Se enseñan los conceptos básicos de programación y se realizan ejercicios prácticos para aprender a programar.',
       tags: ['programación', 'python', 'educación'],
-      link: 'https://python.joanbosch.dev'
+      link: 'https://www.python.joanbosch.dev'
     },
     {
       title: 'Actividades de realidad aumentada con Merge Cube',
